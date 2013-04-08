d0f17e1bc8b05cba1c4c754a82f6f8e280ae7fe0
Michael JasonSmith <mpj17@onlinegroups.net>
2013-04-09 08:51:32 +1000

    Whitespace cleanup

--- gs/group/member/bounce/handler.py
+++ gs/group/member/bounce/handler.py
@@ -1,14 +1,14 @@
 # coding=utf-8
 from datetime import datetime
-from zope.cachedescriptors.property import Lazy
 from zope.component import createObject
 from Products.CustomUserFolder.queries import UserQuery
 from audit import BounceHandlingAuditor, BOUNCE, DISABLE
 from queries import BounceQuery
 
-NO_NOTIFY       = 0
-NOTIFY_BOUNCE   = 1
+NO_NOTIFY = 0
+NOTIFY_BOUNCE = 1
 NOTIFY_DISABLED = 2
+
 
 class Handler(object):
     def __init__(self, site, groupInfo):
@@ -21,22 +21,22 @@ class Handler(object):
         bq = BounceQuery()
         previousBounceDates, daysChecked = bq.previousBounceDates(emailAddress)
         # Record the bounce
-        bq.addBounce(userInfo.id, self.groupInfo.id, self.siteInfo.id, 
+        bq.addBounce(userInfo.id, self.groupInfo.id, self.siteInfo.id,
                      emailAddress)
-        auditor = BounceHandlingAuditor(self.context, userInfo, self.groupInfo, 
+        auditor = BounceHandlingAuditor(self.context, userInfo, self.groupInfo,
                                         self.siteInfo)
         auditor.info(BOUNCE, emailAddress)
-        
+
         retval = NO_NOTIFY
         now = datetime.now()
         bounceDate = now.strftime("%Y%m%d")
         if bounceDate not in previousBounceDates:
             previousBounceDates.append(bounceDate)
             retval = NOTIFY_BOUNCE
-        
+
         # If the address is bouncing too much then disable it.
         numBounceDays = len(previousBounceDates)
-        if numBounceDays >= 5: # TODO: make "5" an option
+        if numBounceDays >= 5:  # TODO: make "5" an option
             self.disable_email(userInfo, emailAddress)
             stats = '%d;%d' % (numBounceDays, daysChecked)
             auditor.info(DISABLE, emailAddress, stats)
