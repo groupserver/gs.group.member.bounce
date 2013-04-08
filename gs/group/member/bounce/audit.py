fc8a2ad71d9fa8f63108b452b90605e9d823eb5e
Michael JasonSmith <mpj17@onlinegroups.net>
2013-04-09 08:54:47 +1000

    The final whitespace cleanup in this batch

--- gs/group/member/bounce/audit.py
+++ gs/group/member/bounce/audit.py
@@ -21,66 +21,68 @@ SUBSYSTEM = 'groupserver.BounceHandling'
 import logging
 log = logging.getLogger(SUBSYSTEM)
 
-UNKNOWN        = '0'  # Unknown is always "0"
-BOUNCE         = '1'
-DISABLE        = '2'
+UNKNOWN = '0'  # Unknown is always "0"
+BOUNCE = '1'
+DISABLE = '2'
+
 
 class BounceHandlingAuditEventFactory(object):
     """A Factory for bounce-handling events
-    
+
     DESCRIPTION
         This factory creates events relating to email bounces
     """
     implements(IFactory)
 
-    title=u'GroupServer Bounce Handling Audit Event Factory'
-    description=u'Creates a GroupServer audit event for email bounce handling'
+    title = u'GroupServer Bounce Handling Audit Event Factory'
+    description = u'Creates a GroupServer audit event for email bounce handling'
 
     def __call__(self, context, event_id, code, date,
         userInfo, instanceUserInfo, siteInfo, groupInfo,
         instanceDatum='', supplementaryDatum='', subsystem=''):
-        
+
         assert subsystem == SUBSYSTEM, 'Subsystems do not match'
-        
+
         if (code == BOUNCE):
-            event = BounceEvent(context, event_id, date, 
+            event = BounceEvent(context, event_id, date,
               instanceUserInfo, siteInfo, groupInfo, instanceDatum)
         elif (code == DISABLE):
-            event = DisableEvent(context, event_id, date, 
-              instanceUserInfo, siteInfo, groupInfo, instanceDatum, 
+            event = DisableEvent(context, event_id, date,
+              instanceUserInfo, siteInfo, groupInfo, instanceDatum,
               supplementaryDatum)
         else:
             # The catch-all
-            event = BasicAuditEvent(context, event_id, UNKNOWN, date, 
-              userInfo, None, siteInfo, groupInfo, 
+            event = BasicAuditEvent(context, event_id, UNKNOWN, date,
+              userInfo, None, siteInfo, groupInfo,
               instanceDatum, supplementaryDatum, SUBSYSTEM)
         assert event
         return event
-    
+
     def getInterfaces(self):
         return implementedBy(BasicAuditEvent)
+
 
 class BounceEvent(BasicAuditEvent):
     ''' An audit-trail event representing an email-bounce.
     '''
-    def __init__(self, context, id, d, userInfo, siteInfo, 
+    def __init__(self, context, id, d, userInfo, siteInfo,
                  groupInfo, instanceDatum):
-        BasicAuditEvent.__init__(self, context, id, BOUNCE, d, 
-          None, userInfo, siteInfo, groupInfo, instanceDatum, 
+        BasicAuditEvent.__init__(self, context, id, BOUNCE, d,
+          None, userInfo, siteInfo, groupInfo, instanceDatum,
           None, SUBSYSTEM)
-        
+
     def __str__(self):
         """Display the event as a string, in such a way that it
         will be useful for the standard Python log.
         """
         retval = u'Bounce detected for %s (%s) in group %s (%s) '\
           u'on site %s (%s) for email address <%s>' %\
-          (self.instanceUserInfo.name, self.instanceUserInfo.id, 
+          (self.instanceUserInfo.name, self.instanceUserInfo.id,
            self.groupInfo.name, self.groupInfo.id,
            self.siteInfo.name, self.siteInfo.id,
            self.instanceDatum, )
         return retval.encode('ascii', 'ignore')
-    
+
     @property
     def xhtml(self):
         """Display the event as string, with XHTML markup, in such
@@ -90,11 +92,12 @@ class BounceEvent(BasicAuditEvent):
           self.code
         retval = u'<span class="%s">Email delivery from %s to '\
           u'<code class="email">%s</code> failed' %\
-          (cssClass, groupInfo_to_anchor(self.groupInfo), 
+          (cssClass, groupInfo_to_anchor(self.groupInfo),
            self.instanceDatum)
         retval = u'%s (%s)' % \
           (retval, munge_date(self.context, self.date))
         return retval
+
 
 class DisableEvent(BasicAuditEvent):
     ''' An audit-trail event representing the system disabling
@@ -102,31 +105,31 @@ class DisableEvent(BasicAuditEvent):
     '''
     def __init__(self, context, id, d, userInfo, siteInfo, groupInfo,
                  instanceDatum, supplementaryDatum):
-        BasicAuditEvent.__init__(self, context, id, DISABLE, d, 
-          None, userInfo, siteInfo, groupInfo, instanceDatum, 
+        BasicAuditEvent.__init__(self, context, id, DISABLE, d,
+          None, userInfo, siteInfo, groupInfo, instanceDatum,
           supplementaryDatum, SUBSYSTEM)
         self.__numBounceDays = self.__numDaysChecked = None
-    
+
     @Lazy
     def numBounceDays(self):
         retval = int(self.supplementaryDatum.split(';')[0])
         return retval
-    
+
     @Lazy
     def numDaysChecked(self):
         retval = int(self.supplementaryDatum.split(';')[1])
         return retval
-        
+
     def __str__(self):
         """ Display the event as a string, in such a way that it
             will be useful for the standard Python log.
         """
         retval = u'Disabled address <%s> for %s (%s). (Detected '\
           u'%d bounces on unique days in the last %d days.)' %\
-          (self.instanceDatum, self.instanceUserInfo.name, 
+          (self.instanceDatum, self.instanceUserInfo.name,
            self.instanceUserInfo.id, self.numBounceDays, self.numDaysChecked)
         return retval.encode('ascii', 'ignore')
-    
+
     @property
     def xhtml(self):
         """ Display the event as string, with XHTML markup, in such
@@ -134,19 +137,22 @@ class DisableEvent(BasicAuditEvent):
         """
         cssClass = u'audit-event groupserver-bounce-event-%s' % \
           self.code
-        retval = u'<span class="%s">Email address <code class="email">%s</code> '\
-          u'disabled after deliveries failed on %d different days in the last %d days.' %\
-          (cssClass, self.instanceDatum, self.numBounceDays, self.numDaysChecked)
+        retval = u'<span class="%s">Email address '\
+            u'<code class="email">%s</code> '\
+            u'disabled after deliveries failed on %d different days in the '\
+            'last %d days.' % (cssClass, self.instanceDatum, self.numBounceDays,
+                                self.numDaysChecked)
         retval = u'%s (%s)' % \
           (retval, munge_date(self.context, self.date))
         return retval
 
+
 class BounceHandlingAuditor(object):
-    """An Auditor for GroupServer 
-    
+    """An Auditor for GroupServer
+
     DESCRIPTION
         This auditor creates an audit trail for the handling of
-        email bounces. The work of creating the actual events is 
+        email bounces. The work of creating the actual events is
         then carried out by "BounceHandlingAuditEventFactory".
     """
     def __init__(self, context, userInfo, groupInfo, siteInfo):
@@ -156,39 +162,39 @@ class BounceHandlingAuditor(object):
         self.userInfo = userInfo
         self.groupInfo = groupInfo
         self.siteInfo = siteInfo
-        
+
         self.queries = AuditQuery()
 
         self.factory = BounceHandlingAuditEventFactory()
-        
+
     def info(self, code, instanceDatum='', supplementaryDatum=''):
         """Log an info event to the audit trail.
 
         DESCRIPTION
             This method logs an event to the audit trail.
-                
+
         ARGUMENTS
             "code"    The code that identifies the event.
-                      
+
             "instanceDatum"
                       Data about the event.
-        
+
         SIDE EFFECTS
             * Creates an ID for the new event,
             * Writes the instantiated event to the audit-table, and
             * Writes the event to the standard Python log.
-        
+
         RETURNS
             None
         """
         d = datetime.now(UTC)
-        eventId = event_id_from_data(self.userInfo, 
+        eventId = event_id_from_data(self.userInfo,
           self.userInfo, self.siteInfo, code, instanceDatum,
           supplementaryDatum)
-        
+
         e = self.factory(self.context, eventId, code, d,
           None, self.userInfo, self.siteInfo, self.groupInfo,
           instanceDatum, supplementaryDatum, SUBSYSTEM)
-          
+
         self.queries.store(e)
         log.info(e)
