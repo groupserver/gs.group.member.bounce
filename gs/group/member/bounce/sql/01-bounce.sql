d01f0329ce4fd3d252e03e19a91516abf76d35a1
Michael JasonSmith <mpj17@onlinegroups.net>
2014-07-31 15:10:31 -0700

    Whitespace cleanup

--- gs/group/member/bounce/sql/01-bounce.sql
+++ gs/group/member/bounce/sql/01-bounce.sql
@@ -3,13 +3,12 @@ SET CLIENT_MIN_MESSAGES = WARNING;
 
 CREATE TABLE bounce (
     -- When the bounce occurred
-    date              TIMESTAMP WITH TIME ZONE  NOT NULL
-                                                DEFAULT NOW(),
+    date      TIMESTAMP WITH TIME ZONE  NOT NULL DEFAULT NOW(),
     -- Whose email is bouncing
-    user_id           TEXT                      NOT NULL,
+    user_id   TEXT                      NOT NULL,
     -- The group that sent the bouncing message
-    group_id          TEXT                      NOT NULL,
-    site_id           TEXT                      NOT NULL,
+    group_id  TEXT                      NOT NULL,
+    site_id   TEXT                      NOT NULL,
     -- The email address that bounced.
-    email             TEXT                      NOT NULL        
+    email     TEXT                      NOT NULL
 );
